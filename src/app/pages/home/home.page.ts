ad4edfaf2f394c7c5e24778ee3fd72a6bcea5dfa
temyrrian7 Artem Bohdan <temyrrian@gmail.com>
2024-12-12 16:09:38 +0100

    delete some useless stuff

--- src/app/pages/home/home.page.ts
+++ src/app/pages/home/home.page.ts
@@ -9,13 +9,5 @@ import { RouterOutlet } from '@angular/router'
   styleUrl: './home.page.scss',
 })
 export default class HomePage {
-  title = 'candidate'
 
-  links = [
-    { title: 'Explore the Docs', link: 'https://angular.dev' },
-    { title: 'Learn with Tutorials', link: 'https://angular.dev/tutorials' },
-    { title: 'CLI Docs', link: 'https://angular.dev/tools/cli' },
-    { title: 'Angular Language Service', link: 'https://angular.dev/tools/language-service' },
-    { title: 'Angular DevTools', link: 'https://angular.dev/tools/devtools' },
-  ]
 }
